0a54ccb4dd57feabb7148695fc1291ec910a6ca4
mattszczp <mattszczp@gmail.com>
2022-06-24 13:08:12 +0200

    LSP configuration

--- nvim/.config/nvim/lua/mattszczp/lsp.lua
+++ nvim/.config/nvim/lua/mattszczp/lsp.lua
@@ -9,6 +9,38 @@ require("nvim-lsp-installer").setup({
     }
 })
 
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
@@ -81,14 +113,34 @@ tabnine:setup({
 	snippet_placeholder = "..",
 })
 
-require('lspconfig')['pyright'].setup {capabilities = capabilities}
-require('lspconfig')['ansiblels'].setup {capabilities = capabilities}
-require('lspconfig')['bashls'].setup {capabilities = capabilities}
-require('lspconfig')['dockerls'].setup {capabilities = capabilities}
-require('lspconfig')['eslint'].setup {capabilities = capabilities}
-require('lspconfig')['golangci_lint_ls'].setup {capabilities = capabilities}
-require('lspconfig')['gopls'].setup {capabilities = capabilities}
-require('lspconfig')['html'].setup {capabilities = capabilities}
+require('lspconfig')['pyright'].setup {
+    capabilities = capabilities,
+    on_attach = on_attach
+}
+require('lspconfig')['ansiblels'].setup {
+    capabilities = capabilities,
+    on_attach = on_attach
+}
+require('lspconfig')['bashls'].setup {
+    capabilities = capabilities,
+    on_attach = on_attach
+}
+require('lspconfig')['dockerls'].setup {
+    capabilities = capabilities,
+    on_attach = on_attach
+}
+require('lspconfig')['eslint'].setup {
+    capabilities = capabilities,
+    on_attach = on_attach
+}
+require('lspconfig')['gopls'].setup {
+    capabilities = capabilities,
+    on_attach = on_attach
+}
+require('lspconfig')['html'].setup {
+    capabilities = capabilities,
+    on_attach = on_attach
+}
 require('lspconfig')['sumneko_lua'].setup {
     settings = {
         Lua = {
@@ -97,7 +149,11 @@ require('lspconfig')['sumneko_lua'].setup {
             }
         }
     },
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach
 }
 require('lspconfig')['tflint'].setup {capabilities = capabilities}
-require('lspconfig')['tsserver'].setup {capabilities = capabilities}
+require('lspconfig')['tsserver'].setup {
+    capabilities = capabilities,
+    on_attach = on_attach
+}
