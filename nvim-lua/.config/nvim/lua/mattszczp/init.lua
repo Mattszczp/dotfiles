ff862cb02baf461651d0bc3458077fef5c5b211f
mattszczp <mattszczp@gmail.com>
2022-07-23 00:15:30 +0200

    Colors

--- nvim-lua/.config/nvim/lua/mattszczp/init.lua
+++ nvim-lua/.config/nvim/lua/mattszczp/init.lua
@@ -22,3 +22,9 @@ vim.g.gruvbox_contrast_dark = "hard"
 vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_transparent = true
 vim.opt.background = "dark"
+vim.api.nvim_set_hl(0, "SignColumn", {bg = "none",})
+vim.api.nvim_set_hl(0, "ColorColumn", {ctermbg = 0, bg = "#555555",})
+vim.api.nvim_set_hl(0, "CursorLineNR", {bg = "None",})
+vim.api.nvim_set_hl(0, "Normal", {bg  = "None",})
+vim.api.nvim_set_hl(0, "LineNr", {fg = "#5eacd3",})
+vim.api.nvim_set_hl(0, "netrwDir", {fg = "#5eacd3",})
