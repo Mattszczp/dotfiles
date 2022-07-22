a706e9cf92ab60407f444e62a6859b02a5f5d556
mattszczp <mattszczp@gmail.com>
2022-07-22 11:38:28 +0200

    transparent Tokyo

--- nvim-lua/.config/nvim/lua/mattszczp/init.lua
+++ nvim-lua/.config/nvim/lua/mattszczp/init.lua
@@ -18,3 +18,7 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.cmd("colorscheme tokyonight")
+vim.g.gruvbox_contrast_dark = "hard"
+vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_transparent = true
+vim.opt.background = "dark"
