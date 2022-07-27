3fd4dfd4ac97ce4a37f8a301668fc8438710df3f
Mateusz Szczepański <mattszczp@gmail.com>
2022-07-27 16:03:50 +0200

    Telescope config

--- nvim-lua/.config/nvim/lua/mattszczp/keymap.lua
+++ nvim-lua/.config/nvim/lua/mattszczp/keymap.lua
@@ -1,4 +1,10 @@
 local nnoremap = require("mattszczp.remap").nnoremap
-
+local telescope = require("telescope.builtin")
+--netrw
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 
+--Telescope
+nnoremap("<leader>ff", telescope.find_files())
+nnoremap("<leader>fg", telescope.live_grep())
+nnoremap("<leader>fb", telescope.buffers())
+nnoremap("<leader>fh", telescope.help_tags())
