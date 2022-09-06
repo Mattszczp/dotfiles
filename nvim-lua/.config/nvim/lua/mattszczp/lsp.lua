70b2180d69c4122c179a2a3f24cb23244101afae
Mateusz Szczepański <mateusz.szczepanski@applica.ai>
2022-09-06 08:51:14 +0200

    add yamlls lsp

--- nvim-lua/.config/nvim/lua/mattszczp/lsp.lua
+++ nvim-lua/.config/nvim/lua/mattszczp/lsp.lua
@@ -147,3 +147,11 @@ require('lspconfig').rust_analyzer.setup(config())
 require('lspconfig').cssls.setup(config())
 require('lspconfig').emmet_ls.setup(config())
 require('lspconfig').tailwindcss.setup(config())
+require('lspconfig').yamlls.setup(config({
+    yaml = {
+        schemas = {
+            ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
+            ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "/.gitlab/*.yml"
+        }
+    }
+}))
