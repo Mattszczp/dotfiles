986d122ca30735c6275d90b506d917fc8998a542
Mateusz Szczepański <matt@kurtwood.me>
2024-03-19 23:38:59 +0100

    add lvim config with dap and formatters

---
+++ lvim/.config/lvim/config.lua
@@ -0,0 +1,20 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.builtin.dap.active = true
+
+lvim.plugins = {
+  { "leoluz/nvim-dap-go" }
+}
+
+require('dap-go').setup()
+
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { name = "gofmt" },
+  { name = "goimports" }
+}
+lvim.format_on_save.enabled = true
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,7 +1,11 @@
 export ZSH="$HOME/.oh-my-zsh"
-export EDITOR='lvim'
+export EDITOR='vim'
 ZSH_THEME="robbyrussell"
 plugins=(git macos aliases python docker git-auto-fetch golang helm kubectl kubectx tmux zsh-autosuggestions)
 alias vim='lvim'
 source $ZSH/oh-my-zsh.sh
 eval "$(rtx activate zsh)"
+source "$HOME/.cargo/env"
+export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.local/share/bob/nvim-bin:$PATH
+export PATH=~/.npm-global/bin:$PATH
